26f6461163714883319fcd746e4be8a1f37495a6
Naveen <70085321+naveen701526@users.noreply.github.com>
2024-07-26 10:20:27 +0530

    Create L5PreOrderTraversalBinaryTree.java

---
+++ L5PreOrderTraversalBinaryTree.java
@@ -0,0 +1,44 @@
+public class L5PreOrderTraversalBinaryTree {
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(13);
+        root.left.right = new Node(15);
+        root.right.left = new Node(5);
+        root.right.right = new Node(9);
+        preOrderTraversal(root);
+        /*
+        tree structure:
+                      1
+                   /     \
+                  2       3
+                 / \     /  \
+               13   15  5    9
+         */
+    }
+
+    private static void preOrderTraversal(Node root) {
+        if (root == null) return;
+        System.out.println(root.data);
+        preOrderTraversal(root.left);
+        preOrderTraversal(root.right);
+    }
+}
+
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+    }
+
+    Node(int data, Node left, Node right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
